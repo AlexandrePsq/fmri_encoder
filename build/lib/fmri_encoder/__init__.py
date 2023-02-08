32dcfd562cfc519747bfd31ab1f26f3253f0a9a4
AlexandrePsq <alexandre-pasquiou@orange.fr>
2023-02-08 17:42:44 +0100

    remove files

--- build/lib/fmri_encoder/__init__.py
+++
@@ -1,9 +0,0 @@
-from .encoder import Encoder
-from .features import (
-    FMRIPipe,
-    FeaturesPipe,
-    FeatureSelector,
-    DesignMatrixBuilder,
-    DimensionReductor
-)
-from .ridge import CustomRidge
